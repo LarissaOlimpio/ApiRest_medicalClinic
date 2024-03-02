83611ec87639b76705b0a327d9b590a6de92eea5
larissaolimpio <lrsolimpio@gmail.com>
2024-03-02 16:37:36 -0300

    Create package and class users

---
+++ src/main/java/doctors/alura/domain/users/Users.java
@@ -0,0 +1,21 @@
+package doctors.alura.domain.users;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Table(name = "users")
+@Entity(name = "Users")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Users {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String login;
+    private String password;
+}
