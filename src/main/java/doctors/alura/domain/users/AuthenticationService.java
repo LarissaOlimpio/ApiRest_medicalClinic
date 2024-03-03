4867552bbe6c6a58a91cb9673e3e1058fe8977c1
larissaolimpio <lrsolimpio@gmail.com>
2024-03-03 19:46:24 -0300

    Create: interface userRepository and create class authenticationService

---
+++ src/main/java/doctors/alura/domain/users/AuthenticationService.java
@@ -0,0 +1,16 @@
+package doctors.alura.domain.users;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthenticationService implements UserDetailsService {
+
+    private UserRepository repository;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return repository.findByLogin(username);
+    }
+}
---
+++ src/main/java/doctors/alura/domain/users/UserRepository.java
@@ -0,0 +1,8 @@
+package doctors.alura.domain.users;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public interface UserRepository extends JpaRepository<Users,Long> {
+    UserDetails findByLogin(String login);
+}
