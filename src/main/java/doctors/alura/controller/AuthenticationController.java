160fec2284270714929644f4015aee8c2a17a28a
larissaolimpio <lrsolimpio@gmail.com>
2024-03-15 16:24:55 -0300

    Create: environment variable about token security and create DTO to DataTokenJWT

--- src/main/java/doctors/alura/controller/AuthenticationController.java
+++ src/main/java/doctors/alura/controller/AuthenticationController.java
@@ -2,13 +2,13 @@ package doctors.alura.controller;
 
 import doctors.alura.domain.users.DataAuthentication;
 import doctors.alura.domain.users.Users;
+import doctors.alura.infra.security.DataTokenJwt;
 import doctors.alura.infra.security.TokenService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -23,9 +23,10 @@ public class AuthenticationController {
 
     @PostMapping
     public ResponseEntity login(@RequestBody @Valid DataAuthentication data){
-        var token = new UsernamePasswordAuthenticationToken(data.login(),data.password());
-        var authentication = manager.authenticate(token);
-        return ResponseEntity.ok(tokenService.createToken((Users) (authentication.getPrincipal())));
+        var tokenAuthentication = new UsernamePasswordAuthenticationToken(data.login(),data.password());
+        var authentication = manager.authenticate(tokenAuthentication);
+        var tokenJWT =tokenService.createToken((Users) (authentication.getPrincipal()));
+        return ResponseEntity.ok(new DataTokenJwt(tokenJWT));
 
     }
 }
---
+++ src/main/java/doctors/alura/infra/security/DataTokenJwt.java
@@ -0,0 +1,4 @@
+package doctors.alura.infra.security;
+
+public record DataTokenJwt(String tokenJWT) {
+}
