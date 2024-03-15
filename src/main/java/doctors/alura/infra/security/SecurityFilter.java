a586dd6a4f945eb93795ad60b695bacf4e93dbe2
larissaolimpio <lrsolimpio@gmail.com>
2024-03-15 17:28:51 -0300

    Create: method recoverToken

--- src/main/java/doctors/alura/infra/security/SecurityFilter.java
+++ src/main/java/doctors/alura/infra/security/SecurityFilter.java
@@ -12,6 +12,16 @@ import java.io.IOException;
 public class SecurityFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        var tokenJwt = recoverToken(request);
         filterChain.doFilter(request,response);
+    }
+
+    private String recoverToken(HttpServletRequest request) {
+        var authorizarionHeader = request.getHeader("Authorization");
+        if(authorizarionHeader == null){
+            throw new RuntimeException("Token JWT not sent in Authorization Header");
+
+        }
+        return authorizarionHeader.replace("Bearer","");
     }
 }
