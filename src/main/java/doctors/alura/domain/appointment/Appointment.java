97f6966a4cbf530fdfc5ce82d58b1c87f7ed8bfc
larissaolimpio <lrsolimpio@gmail.com>
2024-03-21 17:29:26 -0300

    Create: table and entity Appointment

---
+++ src/main/java/doctors/alura/domain/appointment/Appointment.java
@@ -0,0 +1,35 @@
+package doctors.alura.domain.appointment;
+
+import doctors.alura.domain.doctors.Doctors;
+import doctors.alura.domain.patients.Patients;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Table(name = "appointment")
+@Entity(name = "Appointment")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+
+public class Appointment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "doctor_id")
+    private Doctors doctor;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "patient_id")
+    private Patients patient;
+
+    private LocalDateTime data;
+
+}
