0820a53151030aae00fcbc7db27a13d6ec94a4c6
larissaolimpio <lrsolimpio@gmail.com>
2024-03-21 16:00:49 -0300

    Delete: class desnecessary

--- src/main/java/doctors/alura/domain/users/UserCreateAccountRepository.java
+++
@@ -1,6 +0,0 @@
-package doctors.alura.domain.users;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserCreateAccountRepository extends JpaRepository<UserCreateAccount, Long> {
-}
