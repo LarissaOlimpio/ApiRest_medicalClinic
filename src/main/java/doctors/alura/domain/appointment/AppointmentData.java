3c39945089887c66ccfcd1d30250b15c73aecc38
larissaolimpio <lrsolimpio@gmail.com>
2024-03-21 17:15:03 -0300

    Refactor data received in DTO AppointmentData

--- src/main/java/doctors/alura/domain/appointment/AppointmentData.java
+++ src/main/java/doctors/alura/domain/appointment/AppointmentData.java
@@ -1,4 +1,19 @@
 package doctors.alura.domain.appointment;
 
-public record AppointmentData() {
+import doctors.alura.domain.doctors.Specialty;
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.NotNull;
+
+import java.time.LocalDateTime;
+
+public record AppointmentData(
+        @NotNull
+        Long idDoctor,
+        @NotNull
+        Long idPatient,
+        @NotNull
+        @Future
+        LocalDateTime data,
+        Specialty specialty
+                              ) {
 }
