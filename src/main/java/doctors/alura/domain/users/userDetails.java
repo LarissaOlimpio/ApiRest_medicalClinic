ac42db2e71b508c182e08a3046ea30ca68c35172
larissaolimpio <lrsolimpio@gmail.com>
2024-03-22 13:09:56 -0300

    Refactor: field email in return users

--- src/main/java/doctors/alura/domain/users/userDetails.java
+++ src/main/java/doctors/alura/domain/users/userDetails.java
@@ -3,9 +3,9 @@ package doctors.alura.domain.users;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 
-public record userDetails(String name, String login, String phone, String password, String cpf, String userType) {
+public record userDetails(String name, String login, String phone, String email, String password, String cpf, String userType) {
     public userDetails(Users user){
-        this(user.getName(), user.getLogin(), user.getPhone(), user.getPassword(), user.getCpf(), user.getUserType());
+        this(user.getName(), user.getLogin(), user.getPhone(), user.getEmail(),user.getPassword(), user.getCpf(), user.getUserType());
 
     }
 
