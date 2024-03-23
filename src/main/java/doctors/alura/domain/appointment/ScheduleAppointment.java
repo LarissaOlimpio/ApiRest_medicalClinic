1060c56fe3245606f35579dd4ba70c8e806f2a94
larissaolimpio <lrsolimpio@gmail.com>
2024-03-23 16:31:56 -0300

    Create: query sql select random doctor

--- src/main/java/doctors/alura/domain/appointment/ScheduleAppointment.java
+++ src/main/java/doctors/alura/domain/appointment/ScheduleAppointment.java
@@ -1,8 +1,10 @@
 package doctors.alura.domain.appointment;
 
 import doctors.alura.domain.CustomValidationException;
+import doctors.alura.domain.doctors.Doctors;
 import doctors.alura.domain.doctors.DoctorsRepository;
 import doctors.alura.domain.patients.PatientsRepository;
+import jakarta.validation.ValidationException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -22,10 +24,23 @@ public class ScheduleAppointment {
         if (data.idDoctor() != null && !doctorsRepository.existsById(data.idDoctor())) {
             throw  new CustomValidationException("ID doctor not exist");
         }
-        var doctor = doctorsRepository.findById(data.idDoctor()).get();
-        var patient = patientsRepository.findById(data.idPatient()).get();
+        var doctor = chooseDoctor(data);
+        var patient = patientsRepository.getReferenceById(data.idPatient());
 
         var appointment = new Appointment( null, doctor, patient, data.data());
         repository.save(appointment);
+        System.out.println(appointment);
+    }
+
+    private Doctors chooseDoctor(AppointmentData data) {
+        if(data.idDoctor() != null){
+            return doctorsRepository.getReferenceById(data.idDoctor());
+
+        }
+        if(data.specialty() == null){
+            throw new ValidationException("Specialty is mandatory when doctor is not chosen");
+
+        }
+        return doctorsRepository.chooseRandomDoctor(data.specialty(), data.data());
     }
 }
--- src/main/java/doctors/alura/domain/doctors/DoctorsRepository.java
+++ src/main/java/doctors/alura/domain/doctors/DoctorsRepository.java
@@ -2,7 +2,26 @@ package doctors.alura.domain.doctors;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.domain.Page;
+import org.springframework.data.jpa.repository.Query;
+
+import java.time.LocalDateTime;
 
 public interface DoctorsRepository extends JpaRepository<Doctors, Long> {
     Page<Doctors> findAllByActiveTrue(Pageable pagination);
+    @Query("""
+            select d from Doctor d
+            where
+            d.active = 1
+            and
+            d.specialty = :specialty
+            and
+            d.id not in (
+                select ap.doctor.id from Appointment ap
+                where
+                ap.data = :data
+            )
+            order by rand()
+            limit 1
+            """)
+    Doctors chooseRandomDoctor(Specialty specialty, LocalDateTime data);
 }
