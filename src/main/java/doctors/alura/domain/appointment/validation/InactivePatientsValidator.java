0fe8cdea97c36f192d91abd7d86bff2efe59438e
larissaolimpio <lrsolimpio@gmail.com>
2024-03-28 17:45:55 -0300

    Create: class about inactive patients validator

--- src/main/java/doctors/alura/domain/appointment/validation/InactivePatientsValidator.java
+++ src/main/java/doctors/alura/domain/appointment/validation/InactivePatientsValidator.java
@@ -1,9 +1,16 @@
 package doctors.alura.domain.appointment.validation;
 
+import doctors.alura.domain.CustomValidationException;
 import doctors.alura.domain.appointment.ConsultationData;
+import doctors.alura.domain.patients.PatientsRepository;
 
 public class InactivePatientsValidator {
+    private PatientsRepository repository;
     public void validator(ConsultationData data){
+        boolean PatientIsActive = repository.findActiveById(data.idPatient());
+        if(!PatientIsActive){
+            throw new CustomValidationException("Appointment can't be schedule with Inactive patients");
+        }
 
     }
 }
--- src/main/java/doctors/alura/domain/patients/PatientsRepository.java
+++ src/main/java/doctors/alura/domain/patients/PatientsRepository.java
@@ -2,7 +2,16 @@ package doctors.alura.domain.patients;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface PatientsRepository  extends JpaRepository <Patients,Long>{
     Page<Patients>findAllByActiveTrue(Pageable pagination);
+
+    @Query("""
+            select p.active
+            from Patient p
+            where
+            p.id = :id
+            """)
+    boolean findActiveById(Long idPatient);
 }
