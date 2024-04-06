d848edefe1f8306799af131b3758d8a0185e02a5
larissaolimpio <lrsolimpio@gmail.com>
2024-04-06 11:36:22 -0300

    Add: contact info in spring doc

--- src/main/java/doctors/alura/infra/springdoc/SpringDocConfigurations.java
+++ src/main/java/doctors/alura/infra/springdoc/SpringDocConfigurations.java
@@ -2,6 +2,8 @@ package doctors.alura.infra.springdoc;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,7 +15,13 @@ public class SpringDocConfigurations {
         return new OpenAPI()
                 .components(new Components()
                         .addSecuritySchemes("bearer-key",
-                                new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")));
+                                new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")))
+                .info(new Info()
+                .title("Medical Clinic")
+                .description("api rest of the medical clinical application contains CRUD functionalities for doctors and patients, as well as scheduling and canceling appointments")
+                .contact(new Contact()
+                        .name("Time Backend")));
+
     }
 
 
